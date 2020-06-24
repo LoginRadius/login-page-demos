1194e86d7391d69c9952c524bad979a84c2c2652
ankit singh <ankit.singh@loginradius.com>
2020-06-24 16:25:50 +0530

    Added php IDx demo

--- php-idx-demo/demo/js/options.js
+++ php-idx-demo/demo/js/options.js
@@ -1,5 +1,5 @@
 var commonOptions = {};
-commonOptions.apiKey = "<LoginRadius API Key";
+commonOptions.apiKey = "<LoginRadius API Key>";
 commonOptions.appName = "<App Name>";
 const url = window.location.href;
 const params = url.split("?")[1];
