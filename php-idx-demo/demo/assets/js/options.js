f4d02d926a812774bc3b2ba15e2682a378680160
LoginRadius <developers@loginradius.com>
2022-10-10 13:15:17 +0530

    Updated IDX Demo

--- php-idx-demo/demo/assets/js/options.js
+++
@@ -1,7 +0,0 @@
-var commonOptions = {};
-commonOptions.apiKey = "<LoginRadius API Key>";
-commonOptions.appName = "<App Name>";
-const url = window.location.href;
-const params = url.split("?")[1];
-const domainName = url.substring(0, url.lastIndexOf("/"));
-
